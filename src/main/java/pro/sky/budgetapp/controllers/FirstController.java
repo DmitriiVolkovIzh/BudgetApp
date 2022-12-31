62c74c924ea26f327fd020a93ad4b7ba2f356062
DmitriiVolkovIzh <booyakaa@mail.ru>
2022-12-31 12:21:40 +0400

    1 commit

--- src/main/java/pro/sky/budgetapp/controllers/FirstController.java
+++ src/main/java/pro/sky/budgetapp/controllers/FirstController.java
@@ -1,2 +1,23 @@
-package pro.sky.budgetapp.controllers;public class FirstController {
+package pro.sky.budgetapp.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class FirstController {
+
+    @GetMapping
+    public String getApp(){
+        return "Приложение запущено!";
+    }
+
+    @GetMapping("/info")
+    public String page(){
+        return "ФИО: Волков Дмитрий Петрович.**** \n" +
+                "Название проекта: BudgetApp.****\n " +
+                "Дата создания: 31.12.2022.**** \n" +
+                "Приложение для формирования бюджета";
+    }
 }
